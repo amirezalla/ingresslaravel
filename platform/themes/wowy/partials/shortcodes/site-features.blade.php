f3b7f7605760ba57ac9470e057c54d948d9062a7
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2024-01-13 18:35:03 +0100

    Update site-features.blade.php

--- platform/themes/wowy/partials/shortcodes/site-features.blade.php
+++ platform/themes/wowy/partials/shortcodes/site-features.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             @for ($i = 1; $i <= 5; $i++)
                 @if (clean($shortcode->{'title' . $i}))
-                    <div class="col-lg-3 col-md-6 mb-md-3 mb-lg-0">
+                    <div class="col-lg-3 col-md-6 mb-md-3 mb-lg-0" style="background: rgb(8, 25, 56)">
                         <div class="banner-left-icon d-flex align-items-center wow fadeIn animated h-100 {{ $headerStyle == 'header-style-2' ? 'style-2' : '' }}">
                             <div class="banner-icon">
                                 <img src="{{ RvMedia::getImageUrl($shortcode->{'icon' . $i}, null, false, RvMedia::getDefaultImage()) }}" alt="icon">
