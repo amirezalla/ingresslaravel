789fc4adc88080d1515c40d208b22894b93cff1c
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-26 21:54:02 +0200

    Update product.blade.php

--- platform/themes/wowy/views/ecommerce/product.blade.php
+++ platform/themes/wowy/views/ecommerce/product.blade.php
@@ -380,7 +380,8 @@
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="styles.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script>
