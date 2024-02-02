9bc73b89ccf2f725a1337b25bf1880065dbeae89
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-02 13:26:45 +0200

    Update downloads.blade.php

--- platform/plugins/ecommerce/resources/views/themes/customers/orders/downloads.blade.php
+++ platform/plugins/ecommerce/resources/views/themes/customers/orders/downloads.blade.php
@@ -3,15 +3,20 @@
     @php
         use Botble\Ecommerce\Models\Product;
         $user_eth = request()->user('customer')->eth_address;
-        $products = Product::where('owner_eth_address', $user_eth)->get();
+        $products = Product::where('owner_eth_address', $user_eth)->paginate($perPage);
     @endphp
     <div class="row">
-        @foreach ($products as $product)
+        @forelse ($products as $product)
             <div class="col-4">
                 @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-minted-item',
                     $product)
             </div>
-        @endforeach
+        @empty
+            <div>{{ __('No Minted NFTs found') }}</div>
+        @endforelse
 
+    </div>
+    <div class="pagination-wrapper">
+        {{ $products->links() }}
     </div>
 @endsection
