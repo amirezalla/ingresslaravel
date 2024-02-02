faf8e30abc692ea096bdc404f9d2e3bc27cf8472
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-02 13:08:17 +0200

    Update downloads.blade.php

--- platform/plugins/ecommerce/resources/views/themes/customers/orders/downloads.blade.php
+++ platform/plugins/ecommerce/resources/views/themes/customers/orders/downloads.blade.php
@@ -4,15 +4,14 @@
         use Botble\Ecommerce\Models\Product;
         $user_eth = request()->user('customer')->eth_address;
         $products = Product::where('owner_eth_address', $user_eth);
-        $products = Product::where('owner_eth_address', $user_eth)
-            ->get()
-            ->toArray();
     @endphp
     <div class="row">
-        <div class="col-3">
+        @foreach ($products as $product)
+            <div class="col-3">
+                @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-minted-item',
+                    $product)
+            </div>
+        @endforeach
 
-            @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-minted-item',
-                $products)
-        </div>
     </div>
 @endsection
